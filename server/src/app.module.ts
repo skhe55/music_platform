ca3ceb486a6ae30bc7613e1a685068652e3814f4
skhe55 <flashback2019@mail.ru>
2021-11-01 21:44:32 +0700

    added ServerStaticModule in order to serve static content

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { FileModule } from "./file/file.module";
 import { TrackModule } from "./track/track.module";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
-    imports: [TrackModule],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, '..', 'client'),
+        }),
+        MongooseModule.forRoot('mongodb+srv://user:user@cluster0.tn4sn.mongodb.net/music-platform?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule
+    ],
 })
 
 export class AppModule { };
