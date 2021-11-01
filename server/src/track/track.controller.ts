6428d51ad736b63bf944770827aec733db2cb38a
skhe55 <flashback2019@mail.ru>
2021-11-01 21:47:31 +0700

    added this req: search(get), listen(post) and getOne now receives 2 param: count & offset

--- server/src/track/track.controller.ts
+++ server/src/track/track.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Query, UploadedFiles, UseInterceptors } from "@nestjs/common";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
 import { ObjectId } from "mongoose";
 import { CreateCommentDto } from "./dto/create-comment.dto";
 import { CreateTrackDto } from "./dto/create-track.dto";
@@ -10,13 +11,24 @@ export class TrackController {
     constructor(private trackService: TrackService) { }
 
     @Post()
-    create(@Body() dto: CreateTrackDto) {
-        return this.trackService.create(dto);
+    @UseInterceptors(FileFieldsInterceptor([
+        { name: 'picture', maxCount: 1 },
+        { name: 'audio', maxCount: 1 },
+    ]))
+    create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+        const { picture, audio } = files;
+        return this.trackService.create(dto, picture[0], audio[0]);
     }
 
     @Get()
-    getAll() {
-        return this.trackService.getAll();
+    getAll(@Query('count') count: number,                              //// number of received tracks
+        @Query('offset') offset: number) {                             //// offset between them
+        return this.trackService.getAll(count, offset);
+    }
+
+    @Get('/search')
+    search(@Query('query') query: string) {
+        return this.trackService.search(query);
     }
 
     @Get(':id')
@@ -32,5 +44,10 @@ export class TrackController {
     @Post('/comment')
     addComent(@Body() dto: CreateCommentDto) {
         return this.trackService.addComment(dto);
+    }
+
+    @Post('/listen/:id')
+    listen(@Param('id') id: ObjectId) {
+        return this.trackService.listen(id);
     }
 }
